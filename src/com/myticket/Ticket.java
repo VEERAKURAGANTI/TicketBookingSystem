698015e2781a171ad9d2f1d0df91f76111f72af5
KURAGANTI VEERANJANEYULU <164483198+VEERAKURAGANTI@users.noreply.github.com>
2026-04-15 21:25:17 +0530

    Clarify bookedUserId with a comment
    Added comment to clarify the purpose of bookedUserId.

--- src/com/myticket/Ticket.java
+++ src/com/myticket/Ticket.java
@@ -11,7 +11,7 @@ public class Ticket implements Serializable {
     private String destinationPoint;
     private int seatNo;
     private int cost; 
-    private int bookedUserId; 
+    private int bookedUserId; // user who booked this ticket  
 
     private static final Random RANDOM = new Random();
 
