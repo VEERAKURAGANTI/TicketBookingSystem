d5f3ca83fe0121e9a5b0edf52e61684e68211d70
KURAGANTI VEERANJANEYULU <164483198+VEERAKURAGANTI@users.noreply.github.com>
2026-07-07 22:23:09 +0530

    Fix comment typo in TicketBookingSystem.java

--- src/com/myapp/TicketBookingSystem.java
+++ src/com/myapp/TicketBookingSystem.java
@@ -9,7 +9,7 @@ import com.myuser.User;
 public class TicketBookingSystem {
 
     private  ArrayList<User> users = new ArrayList<User>();
-    private  ArrayList<Ticket> tickets = new ArrayList<Ticket>(); //globle list
+    private  ArrayList<Ticket> tickets = new ArrayList<Ticket>(); //
 
     //create the user
     public void createUser(User u) {
